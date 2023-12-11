514b7dba0f60e63421e539b74a6ab3f04e47626c
terzoc <cterzo001@gmail.com>
2023-12-11 17:38:46 -0500

    Added choice for color blindness type and complete deficiency filter

--- colorblind.py
+++ colorblind.py
@@ -11,11 +11,36 @@ cap.set(4, frameHeight)
 #Brightness
 cap.set(10,150) 
 
+def validateInput(choice):
+    if(choice.isnumeric()):
+        choice = int(choice)
+        if(choice > 3 or choice < 1):
+            choice = validateInput(input("Sorry that is not a valid selection please try again: "))
+    else:
+        choice = validateInput(input("Sorry that is not a valid selection please try again: "))
+    return choice
+
+print("What kind of color blindness would you like to simulate?")
+print("Enter 1 for Red-green deficiency")
+print("Enter 2 for Blue-yellow deficiency")
+print("Enter 3 for Complete color deficiency")
+choice = validateInput(input(""))
+
+windowNames = {1 : "Red-Green Deficiency", 2: "Blue-yellow Deficiency", 3: "Complete color Deficiency"}
+
 while True:
     ret, frame = cap.read()
     if ret:
         #frame = cv.copyMakeBorder(frame, 10, 10, 10, 10, cv.BORDER_CONSTANT, None, value = [0,0,255]) #adds a border to webcam display, just for funsies(testing)
-        cv.imshow("Result", frame)
+        match choice:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        cv.imshow(windowNames[choice], frame)
         if cv.waitKey(25) & 0xFF == ord('q'): 
             break
     else:
